b9cbdd4bda0914220346de24f9a8826654863d26
Alper <a.yegenoglu@fz-juelich.de>
2022-03-14 17:57:35 +0100

    Update test_optimizer.py

--- l2l/tests/test_optimizer.py
+++ l2l/tests/test_optimizer.py
@@ -15,8 +15,9 @@ class OptimizerTestCase(unittest.TestCase):
         bench_functs = BenchmarkedFunctions()
         (benchmark_name, benchmark_function), benchmark_parameters = \
             bench_functs.get_function_by_index(function_id, noise=True)
-
-        self.experiment = Experiment(root_dir_path='../../results')
+        home_path =  os.environ.get("HOME")
+        root_dir_path = os.path.join(home_path, 'results')
+        self.experiment = Experiment(root_dir_path=root_dir_path)
         jube_params = {}
         self.trajectory, all_jube_params = self.experiment.prepare_experiment(name='L2L',
                                                                               log_stdout=True,

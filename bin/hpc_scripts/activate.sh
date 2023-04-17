9678c56a5f9c659bc1e1d102889e03b7d3b93968
DeLaVlag <michielvandervlag@gmail.com>
2023-04-17 10:36:16 +0200

    summit23 fix for tqdm bug

--- bin/hpc_scripts/activate.sh
+++ bin/hpc_scripts/activate.sh
@@ -6,3 +6,6 @@ export PYTHONPATH=$PYTHONPATH:$PROJECT/$USER/TVB/tvb_library
 # for sdict which is at this location
 export PYTHONPATH=$PYTHONPATH:$PROJECT/$USER/L2L/l2l/utils
 export PYTHONPATH=$PYTHONPATH:$PROJECT/$USER/L2L
+
+export PYTHONPATH=$PYTHONPATH:$PROJECT/$USER/
+

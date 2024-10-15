a0b0268e3bc696e489605feaf2758253c753a9f8
DeLaVlag <michielvandervlag@gmail.com>
2024-10-15 16:31:39 +0200

    take out bad jube

--- bin/hpc_scripts/activate.sh
+++ bin/hpc_scripts/activate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/env bashasd
 
-module load CUDA Python SciPy-Stack numba JUBE PyCUDA
+module load CUDA Python SciPy-Stack numba PyCUDA
 
 export PYTHONPATH=$PYTHONPATH:$PROJECT/$USER/TVB/tvb_library
 # for sdict which is at this location
